5213429e5ffe9d41170faf67e648cc453d4eb9a4
Casey C. <kc.khchen@gmail.com>
2026-01-27 19:32:47 +0800

    feat: Add bouncing alien animation and interaction

--- script.js
+++ script.js
@@ -34,3 +34,57 @@ const yearSpan = document.getElementById("currentYear");
 if (yearSpan) {
   yearSpan.textContent = new Date().getFullYear();
 }
+
+// Bouncing little alien
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.scrollTo(0, 0);
+const alien = document.getElementById("bouncer");
+
+let x = Math.random() * document.documentElement.clientWidth * 0.8;
+// Make sure alien appears from out of the screen initially
+let y =
+  window.innerHeight +
+  Math.random() * (document.documentElement.scrollHeight - window.innerHeight);
+let dx = (Math.random() - 0.5) * 4;
+let dy = Math.min(2, (Math.random() - 0.5) * 4);
+const imgWidth = 30;
+const imgHeight = 30;
+
+function animate() {
+  const windowWidth = document.documentElement.clientWidth;
+  const windowHeight = document.documentElement.scrollHeight;
+
+  x += dx;
+  y += dy;
+
+  if (x + imgWidth >= windowWidth || x <= 0) {
+    dx = -dx;
+  }
+
+  if (y + imgHeight >= windowHeight || y <= 0) {
+    dy = -dy;
+  }
+
+  alien.style.left = x + "px";
+  alien.style.top = y + "px";
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+alien.addEventListener("click", () => {
+  Swal.fire({
+    width: 400,
+    title: "Congratulations!",
+    html: "You caught the little alien!<br/>(Please let him go. He's scared.)",
+    confirmButtonText: "Alright...",
+    confirmButtonColor: "#10b981",
+    theme: "dark",
+  });
+
+  alien.remove();
+});
